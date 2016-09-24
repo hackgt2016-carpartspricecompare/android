edca37ff74d4d141fbeb43cf7f21ea9e32c3d90c
Joshua King <joshua@antechdevelopment.com>
2016-09-24 10:16:31 -0400

    Added cardview library

--- app/src/main/java/com/hackgt/partspricer/MainActivity.java
+++ app/src/main/java/com/hackgt/partspricer/MainActivity.java
@@ -1,7 +1,14 @@
 package com.hackgt.partspricer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+import static com.hackgt.partspricer.R.id.bottomBar;
 
 public class MainActivity extends AppCompatActivity {
 	@Override
@@ -9,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-//		findViewById(R.id.)
+		com.hackgt.partspricer.PartsFragment partsFragment = new com.hackgt.partspricer.PartsFragment();
+		FrameLayout fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
+
+		FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction().add(R.id.fragment_container, partsFragment).commit();
+
+		BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+		bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+			@Override
+			public void onTabSelected (@IdRes int tabId) {
+
+			}
+		});
 	}
 }
