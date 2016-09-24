b53267c8f5e6892b96d2eb7b6cb5c896dd608bbc
Joshua King <joshua@antechdevelopment.com>
2016-09-24 12:13:50 -0400

    Main activity is now switching fragments

--- app/src/main/java/com/hackgt/partspricer/MainActivity.java
+++ app/src/main/java/com/hackgt/partspricer/MainActivity.java
@@ -1,17 +1,12 @@
 package com.hackgt.partspricer;
 
+import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.FrameLayout;
-
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
-
-import static com.hackgt.partspricer.R.id.bottomBar;
 
 public class MainActivity extends AppCompatActivity implements PartsFragment.OnFragmentInteractionListener {
     @Override
@@ -19,18 +14,20 @@ public class MainActivity extends AppCompatActivity implements PartsFragment.OnF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.app.Fragment partsFragment = new com.hackgt.partspricer.PartsFragment();
+//        android.app.Fragment partsFragment = new com.hackgt.partspricer.PartsFragment();
 //        FrameLayout fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
 
-
-
-        getFragmentManager().beginTransaction().add(R.id.fragment_container, partsFragment).commit();
+//        getFragmentManager().beginTransaction().add(R.id.fragment_container, partsFragment).commit();
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
+                Fragment fragment;
+                if (tabId == R.id.tab_parts) { fragment = new PartsFragment(); }
+                else { fragment = new Fragment(); }
 
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
             }
         });
     }
