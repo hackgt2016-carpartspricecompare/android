e55e3f6055f264c325e4cdeeadda416f02f761f0
Pranathi <pvenigandla@gmail.com>
2016-09-24 11:47:38 -0400

    added Part.java and Category.java

--- app/src/main/java/com/hackgt/partspricer/MainActivity.java
+++ app/src/main/java/com/hackgt/partspricer/MainActivity.java
@@ -1,32 +1,42 @@
 package com.hackgt.partspricer;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
+
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 import static com.hackgt.partspricer.R.id.bottomBar;
 
-public class MainActivity extends AppCompatActivity {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+public class MainActivity extends AppCompatActivity implements PartsFragment.OnFragmentInteractionListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		com.hackgt.partspricer.PartsFragment partsFragment = new com.hackgt.partspricer.PartsFragment();
-		FrameLayout fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
+        android.app.Fragment partsFragment = new com.hackgt.partspricer.PartsFragment();
+//        FrameLayout fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
 
-		FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction().add(R.id.fragment_container, partsFragment).commit();
 
-		BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-		bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-			@Override
-			public void onTabSelected (@IdRes int tabId) {
 
-			}
-		});
-	}
+        getFragmentManager().beginTransaction().add(R.id.fragment_container, partsFragment).commit();
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
---
+++ app/src/main/java/com/hackgt/partspricer/Part.java
@@ -0,0 +1,24 @@
+package com.hackgt.partspricer;
+
+import android.icu.util.ULocale;
+
+/**
+ * Created by Pranathi on 9/24/2016.
+ */
+
+public class Part {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Part(){
+
+
+    }
+}
