c4d6733374a2d0bda51967665fdbef8fe214cd00
Pranathi <pvenigandla@gmail.com>
2016-09-25 00:05:04 -0400

    Added category and part data into DataStore.java

--- app/src/main/java/com/hackgt/partspricer/DataStore.java
+++ app/src/main/java/com/hackgt/partspricer/DataStore.java
@@ -1,8 +1,36 @@
 package com.hackgt.partspricer;
 
+import java.util.ArrayList;
+
 /**
  * Created by Joshua King on 9/24/16.
  */
 public class DataStore {
 
+    private static ArrayList<Category> partCategories;
+
+    static{
+        partCategories = new ArrayList<>();
+
+
+
+        ArrayList<Part> subPartCategoriesAC = new ArrayList<>();
+        subPartCategoriesAC.add(new Part("Element","AC Filter","15781507"));
+        partCategories.add(new Category("AC Filter", subPartCategoriesAC));
+
+        ArrayList<Part> subPartCategoriesBulb = new ArrayList<>();
+        subPartCategoriesBulb.add(new Part("Head Light","Bulb","1302"));
+        subPartCategoriesBulb.add(new Part("Break Light","Bulb","2871"));
+        partCategories.add(new Category("Bulb", subPartCategoriesBulb));
+
+        ArrayList<Part> subPartCategoriesBlade = new ArrayList<>();
+        subPartCategoriesBlade.add(new Part("Blade","Wiper Blade", "13000" ));
+        partCategories.add(new Category("Blade", subPartCategoriesBlade));
+
+        ArrayList<Part> subPartCategoriesCap = new ArrayList<>();
+        subPartCategoriesCap.add(new Part());
+        partCategories.add(new Category("Caps ", subPartCategoriesCap));
+
+
+    }
 }
